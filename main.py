f9e6ec731f8cb2bf41b7392a422692f5a30f9a98
221RDB093 <221RDB093@users.noreply.github.com>
2025-03-19 12:34:45 +0200

    functioning brute force solution on data from files

--- main.py
+++ main.py
@@ -7,26 +7,32 @@ kurš saturēja failā "ints_10.txt" esošās vērtības.
 Par failu nolasīšanu uztraukšos vēlāk
 """
 
-test = [
-        56,
-        53,
-        74,
-        59,
-        11,
-        41,
-        47,
-        46,
-        57,
-        41
-        ]
+ints = [] # saraksts, kurā glabāsies teksta faila vērtības
+
+while True:
+    try:
+        print("Sveiki! Jums jāievada ierakstu skaits failam, kuru vēlaties pārmeklēt!")
+        print("Iespējamās ievades iespējas ir: 10, 100, 1K, 10K, 100K, 1M, 10M")
+        file = input("Ievadiet savu izvēli: ")      # pieprasu lietotājam ievadīt ierakstu daudzumu, kas jāapstrādā 
+        f = open(f"pd1_data/ints_{file}.txt", "r")  # nolasu attiecīgo failu
+        
+        for line in f:                              
+            ints.append(int(line))                  # katra iterācija (jeb rinda) tiek parsēta uz int tipu un pievienota sarakstam
+            
+        break
+    except FileNotFoundError:                       # izņēmums gadījumam, ja lietotāja ievade ir nederīga
+        print("Nepareiza ievade!")
+    
+
+    
 
 maxRange = 0                                # mainīgais, kas saglabās maksimālo iespējamo intervālu
-for i in range(0, len(test)):
-    for j in range(i+1, len(test)):         # iterēju cauri visiem skaitļiem, no kuriem būs jāatņem skaitlis test[i] no iepriekšējā cikla  
-        currentRange = test[j] - test[i]
+for i in range(0, len(ints)):
+    for j in range(i+1, len(ints)):         # iterēju cauri visiem skaitļiem, no kuriem būs jāatņem skaitlis test[i] no iepriekšējā cikla  
+        currentRange = ints[j] - ints[i]
         if currentRange > maxRange:         # pārbaudu, vai patreizējās iterācijas starpība ir lielāka par patreizējo lielāko starpību
-            mazinamais = test[i]
-            mazinatajs = test[j]            # ja jā, tad piefiksēju skaitļus, starp kuriem ir šī starpība
+            mazinamais = ints[i]
+            mazinatajs = ints[j]            # ja jā, tad piefiksēju skaitļus, starp kuriem ir šī starpība
             maxRange = currentRange         # un atjauninu lielāko starpību
             
 print(f"lielākais intervāls ir {maxRange} starp skaitļiem {mazinamais} un {mazinatajs}")
